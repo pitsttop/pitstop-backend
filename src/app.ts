45d8dcf319571281fbea618687bc0678e69bb2af
joao <joaoaraujosiqueira@gmail.com>
2025-11-24 17:29:34 -0300

    Fix CORS

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,15 @@ export const createApp = () => {
   const app = express();
 
   app.use(
-    cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000' }),
+    cors({
+      origin: [
+        'http://localhost:3000',      
+        'http://localhost:5173',      
+        'https://pitstop-frontend.vercel.app', 
+        'https://pitstop-frontend-dh5to3w0m-joao-lucas-araujo-siqueiras-projects.vercel.app'
+      ],
+      credentials: true, 
+    }),
   );
   app.use(morgan('dev'));
   app.use(express.json());
